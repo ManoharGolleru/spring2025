c2382339e69b39d96c3366577c6cd921cbe89361
Chase Murray <cmurray3@buffalo.edu>
2025-02-04 10:27:43 -0500

    speech transcription

--- Projects/speech_transcription/start.sh
+++ Projects/speech_transcription/start.sh
@@ -18,25 +18,27 @@ sleep 1s
 
 # FIXME 3 -- Need to document the directory structure.
 #            Filepaths are hard-coded below
+PWD=$(pwd)
 
 # Start node.js web server
 SCRIPT1="node server_secure.cjs --public"
-gnome-terminal --tab --title "NODE.JS" --working-directory=${HOME}/Projects/speech_transcription/html -- bash -ic "export HISTFILE=${HOME}/.bash_history_junk1; $SCRIPT1; history -s $SCRIPT1; exec bash" 
+gnome-terminal --tab --title "NODE.JS" --working-directory=${PWD}/html -- bash -ic "export HISTFILE=${HOME}/.bash_history_junk1; $SCRIPT1; history -s $SCRIPT1; exec bash" 
 
 # Start WhisperLive Server
 SCRIPT2="python3 run_server.py --port 9091 --backend faster_whisper"
-gnome-terminal --tab --title "FASTER_WHISPER" --working-directory=${HOME}/Projects/speech_transcription/scripts -- bash -ic "export HISTFILE=${HOME}/.bash_history_junk2; $SCRIPT2; history -s $SCRIPT2; exec bash" 
+gnome-terminal --tab --title "FASTER_WHISPER" --working-directory=${PWD}/scripts -- bash -ic "export HISTFILE=${HOME}/.bash_history_junk2; $SCRIPT2; history -s $SCRIPT2; exec bash" 
 
 # Start our main.py node:
 SCRIPT3="python3 main.py"
-gnome-terminal --tab --title "MAIN.PY" --working-directory=${HOME}/Projects/speech_transcription/scripts -- bash -ic "export HISTFILE=${HOME}/.bash_history_junk3; $SCRIPT3; history -s $SCRIPT3; exec bash" 
+gnome-terminal --tab --title "MAIN.PY" --working-directory=${PWD}/scripts -- bash -ic "export HISTFILE=${HOME}/.bash_history_junk3; $SCRIPT3; history -s $SCRIPT3; exec bash" 
 
 
 # Start chromium
 sleep 5s
 # chromium "https://${HOST_IP}:8080/"
 # firefox "https://localhost:8080/"
-chromium "https://localhost:8080/"
+chromium "https://localhost:8080/index.html"
+chromium "https://localhost:8080/admin.html"
 
 
 # Print some info to the screen
